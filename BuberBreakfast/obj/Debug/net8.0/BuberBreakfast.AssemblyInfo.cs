3cce6f8578f2b0e5e18478f72620dfa7b829ecbf
Jack Ooi <thooi998@gmail.com>
2024-02-17 16:44:01 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfoInputs.cache

--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f41cb91e355d35e50da414f3ad6ae718d0546ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9b0bcadc59b0c4e3768864e9692ec5f741600f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
