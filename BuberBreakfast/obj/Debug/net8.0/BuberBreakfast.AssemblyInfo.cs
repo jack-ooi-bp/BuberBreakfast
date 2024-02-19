fd16f00a29f408c36d5a62f6518bf0e97522b88c
jack.ooi <jack.ooi@bp.com>
2024-02-19 19:28:05 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   BuberBreakfast.Contracts/bin/Debug/net8.0/BuberBreakfast.Contracts.dll
    	modified:   BuberBreakfast.Contracts/bin/Debug/net8.0/BuberBreakfast.Contracts.pdb
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.AssemblyInfo.cs
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.AssemblyInfoInputs.cache
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.dll
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.pdb
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/BuberBreakfast.Contracts.sourcelink.json
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/ref/BuberBreakfast.Contracts.dll
    	modified:   BuberBreakfast.Contracts/obj/Debug/net8.0/refint/BuberBreakfast.Contracts.dll
    	modified:   BuberBreakfast/bin/Debug/net8.0/BuberBreakfast.Contracts.dll
    	modified:   BuberBreakfast/bin/Debug/net8.0/BuberBreakfast.Contracts.pdb
    	modified:   BuberBreakfast/bin/Debug/net8.0/BuberBreakfast.dll
    	modified:   BuberBreakfast/bin/Debug/net8.0/BuberBreakfast.exe
    	modified:   BuberBreakfast/bin/Debug/net8.0/BuberBreakfast.pdb
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfoInputs.cache
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.csproj.AssemblyReference.cache
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.dll
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.pdb
    	modified:   BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.sourcelink.json
    	modified:   BuberBreakfast/obj/Debug/net8.0/apphost.exe
    	modified:   BuberBreakfast/obj/Debug/net8.0/ref/BuberBreakfast.dll
    	modified:   BuberBreakfast/obj/Debug/net8.0/refint/BuberBreakfast.dll
    	modified:   Request/GetBreakfastRequest.http

--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a33fe8aef6efbcd9bb7c46b175e55024bb3b26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1371d26c2fb28433671e13eeb9ef55c04ef3bf80")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
